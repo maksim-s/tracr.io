c985efbb42ea0d329176dd2cfa91e928947ffa21
Maksim Stepanenko <maksimus16@gmail.com>
2013-08-03 13:46:00 -0700

    base.html

--- tracr.io/website/frontpage/forms.py
+++ tracr.io/website/frontpage/forms.py
@@ -3,5 +3,6 @@ from django import forms
 class EmailSubscriptionForm(forms.Form):
   email = forms.EmailField(max_length=256,
                            required=True,
-                           label='Leave Your Email')
+                           widget=forms.TextInput(
+                             attrs={'placeholder': 'Leave Your Email'}))
 
