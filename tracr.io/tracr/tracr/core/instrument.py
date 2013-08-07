436a32c7dd999a2707f5bec04d228fa58daed972
Usman Masood <usmanm@outlook.com>
2013-08-07 09:12:00 -0700

    Add instrument model and scope functions
    Differential Revision: http://phabricator.tracr.io/D31

---
+++ tracr.io/tracr/tracr/core/instrument.py
@@ -0,0 +1,34 @@
+import time
+
+from django.db.models import Model
+
+from tracr.core.context import context
+
+def instrument_model(klass, **kwargs):
+  if not issubclass(klass, Model):
+    raise ValueError
+  
+  # Annotate all save calls with their duration.
+  old_save = klass.save
+  def save(self, *args, **kwargs):
+    start_time = time.time()
+    value = old_save(*args, **kwargs)
+    context.annotate('%s:save' % klass.__name__, {
+        'duration': time.time() - start_time
+        })
+    return value
+  klass.save = save
+
+  return klass
+
+def scope(object):
+  def __name__(self, name):
+    self._name = name
+
+  def __enter__(self):
+    scope = context.begin_scope(self._name)
+    return scope
+
+  def __exit__(self, *args, **kwargs):
+    context.leave_scope()
+    return super(scope, self).__exit__(self, *args, **kwargs)
