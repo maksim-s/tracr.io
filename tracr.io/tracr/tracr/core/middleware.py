0857d8b23afaf48e868184ad76450fc7022d1f12
Usman Masood <usmanm@outlook.com>
2013-08-24 17:19:36 -0700

    Clean up middleware stuff and create/destroy context upon start/completion of request/response cycle
    Reviewers: arsen, kkamalov, maksims
    
    Reviewed By: maksims
    
    Differential Revision: http://phabricator.tracr.io/D37

--- tracr.io/tracr/tracr/core/middleware.py
+++ tracr.io/tracr/tracr/core/middleware.py
@@ -1,15 +1,19 @@
 from tracr.conf.loader import Loader
+from tracr.core.context import manager
 
 class TracrMiddleware(object):
   """
   Main middleware that combines all handlers together.
   """
-
   def __init__(self):
     self.loader = Loader()
     self.handlers = self.loader.load_handlers()
 
   def process_request(self, request):
+    # Set up tracr context for request.
+    context = manager.create_context(request)
+    request.trace = context.get_trace()
+
     for handler in self.handlers:
       handler.process_request(request)
 
@@ -32,9 +36,13 @@ class TracrMiddleware(object):
       handler_response = handler.process_response(request, response)
       if handler_response:
         response = handler_response
+
+    # Teardown tracr context.
+    manager.destroy_context()
+    # TODO(usmanm): Ship trace to server.
     return response
 
   def process_exception(self, request, exception):
     for handler in self.handlers:
-      handler.process_request(request)
+      handler.process_exception(request, exception)
 
